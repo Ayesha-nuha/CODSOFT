1e1a2f69e9b9f7916958932e7215b165902f0edf
Ayesha-nuha <ayeshanuha49@gmail.com>
2024-09-23 21:03:50 +0530

    simplecalculator.py
    This program receives two input numbers 'Num 1' and 'Num 2' from the user and also gets the input of the operation to be performed. Then it processes the input and prints the output depending on the operation given by the user.

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,1 +1,22 @@
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+print("Available operations in this simple calculator: '+','-','*','/' ")
+operation=input("Enter the operation to be performed: ")
 
+if operation == '+':
+  print("Result:",a+b)
+  
+elif operation == '-':
+  print("Result:",a-b)
+  
+elif operation == '*':
+  print("Result:",a*b)
+  
+elif operation == '/':
+ if b != 0:
+   print("Result:",a/b)
+ else:
+   print("Undefined")
+   
+else:
+  print("INVALID OPERATION")
